aaa1d47c27a93d6c55ca8072a155de616ab7aeed
BekirLee <nihadbekirli8@gmail.com>
2023-04-16 15:33:01 +0400

    students

--- uygulama_donguler.js
+++ uygulama_donguler.js
@@ -28,21 +28,45 @@ let numbers = [1, 3, 4, 6, 5, 7, 2, 12, 2, 5, 4];
 
 
 
-let products = ["iphone 12", "samsung s22", "iphone 13", "samsung s23","samsung s23","samsung s23"];
+let products = ["iphone 12", "samsung s22", "iphone 13", "samsung s23", "samsung s23", "samsung s23"];
 
 // for (let product of products){
 //     console.log(product.toUpperCase());
 // }
 
-let number = 0;
-for (let product of products) {
-    if(product.includes("samsung"))
-    {
-         number++;
+// let number = 0;
+// for (let product of products) {
+//     if(product.includes("samsung"))
+//     {
+//          number++;
 
-     }
+//      }
+
+// }
+
+
+// console.log(number);
+
+let ort = 0;
+
+let students = [
+    { "ad": "Nihad", "soyad": "Bakirli", "bal": [70, 60, 90] },
+    { "ad": "Amil", "soyad": "Qarayev", "bal": [60, 90, 70] },
+    { "ad": "Ahmad", "soyad": "Selimov", "bal": [80, 40, 80] }
+]
+
+for (let student of students) {
+    let xal = 0;
+    let eded=0;
+    for (let ballar of student.bal) {
+        xal += ballar;
+        eded++;
+    }
+    ort = xal / eded;
+
+    console.log(`${student.ad} ${student.soyad} adinda olan sagirdin ortalamsi : ${xal}`);
+
     
+
+
 }
-
-
-console.log(number);
