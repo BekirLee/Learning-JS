fcdccb2e9fee2912826595fc7a546172a2ab741c
BekirLee <nihadbekirli8@gmail.com>
2023-05-05 06:49:05 +0400

    Update side of progect

--- Uptading_elements/uptade_elements.js
+++ Uptading_elements/uptade_elements.js
@@ -5,9 +5,12 @@ let tasks = [
     { "id": 2, "name": "task2" },
     { "id": 3, "name": "task3" },
     { "id": 4, "name": "task4" },
-
-
 ]
+
+let editid;
+let istaskupdate = false;
+
+const taskinput = document.querySelector("#password");
 displaytask();
 
 function displaytask() {
@@ -30,6 +33,7 @@ function displaytask() {
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                         <li><a onclick="deleteTask(${result.id})" class="dropdown-item" href="#"><i class="fa-solid fa-trash"></i> Del</a></li>
                         <li><a  class="dropdown-item" href="#"><i class="fa-solid fa-pen"></i> Add</a></li>
+                        <li><a onclick='updateTask(${result.id}," ${result.name}")' class="dropdown-item" href="#"><i class="fa-solid fa-trash"></i> Update</a></li>
                         </ul>
                         </div>`;
 
@@ -43,14 +47,29 @@ document.querySelector("#form1").addEventListener("click", newtask);
 
 
 function newtask(event) {
-    let taskinput = document.querySelector("#password");
     if (taskinput.value == "") {
         alert("You should write true text");
     }
     else {
-        tasks.push({ "id": tasks.length + 1, "name": taskinput.value })
-        taskinput.value = "";
-        displaytask();
+        if (!istaskupdate) {
+            //add
+            tasks.push({ "id": tasks.length + 1, "name": taskinput.value })
+            taskinput.value = "";
+            displaytask();
+
+        }
+        else {
+            //update
+            for (let result of tasks) {
+                if (result.id == editid) {
+                    result.name = taskinput.value;
+
+                }
+                istaskupdate = false;
+            }
+
+        }
+
 
     }
     event.preventDefault();
@@ -82,4 +101,11 @@ function deleteTask(id) {
 
 }
 
+function updateTask(taskId, taskName) {
+    editid = taskId;
+    istaskupdate = true;
+    taskinput.value = taskName;
+    taskinput.focus();
+    taskinput.classList.add("active");
+}
 
