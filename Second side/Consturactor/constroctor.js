dbc5593a02a91a0159152e7121934331fcfa7ca7
BekirLee <nihadbekirli8@gmail.com>
2023-05-09 11:10:18 +0400

    deleting comments

--- Second side/Consturactor/constroctor.js
+++ Second side/Consturactor/constroctor.js
@@ -1,27 +1,14 @@
-// let question = {
-//     text: "Which is program language",
-//     options: {
-//         a: "pthon",
-//         b: "css",
-//         c: "html"
 
-//     },
-//     right_answer: "a",
-
-//     control: function (answer) {
-//         return answer === this.right_answer;
-//     }
-// }
-
-// console.log(question.text);
-// console.log(question.control("d"));
-
-
-function Answer(text, options, right_answer){
-    this.text=text;
-    this.options=options;
-    this.right_answer=right_answer;
+function Answer(text, options, right_answer) {
+    this.text = text;
+    this.options = options;
+    this.right_answer = right_answer;
+    this.control = function (answer) {
+        return answer === this.right_answer;
+    }
 
 }
-let question1= new Answer("Which is program language",{a:"python",b:"java",c:"C",} ,"c") 
+let question1 = new Answer("Which is program language", { a: "python", b: "java", c: "C", }, "c");
+console.log(question1.right_answer);
 
+console.log(question1.control("c"));
