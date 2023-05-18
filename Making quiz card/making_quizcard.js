b0995f12b5fcdc008f0aa4d9ad5880b8876eb4ad
BekirLee <nihadbekirli8@gmail.com>
2023-05-18 22:58:24 +0400

    making card for questions

---
+++ Making quiz card/making_quizcard.js
@@ -0,0 +1,47 @@
+function Question(text, options, right_answer) {
+
+
+    this.text = text;
+    this.options = options;
+    this.right_answer = right_answer;
+    this.control = function (answer) {
+        return answer === this.right_answer;
+    }
+
+}
+
+
+
+let questions = [
+    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
+
+
+    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
+
+
+    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
+
+
+]
+
+// /0,undefined,null,'',"", -- false  ////// [],{}
+
+
+function Quiz(questions) {
+    this.questions = questions;
+    this.questionIndex=0;
+}
+
+Quiz.prototype.bring = returnQuiz(questions)
+
+ const quiz =  Quiz.prototype.bring ;
+ document.querySelector(".btn-start").addEventListener("click", function () {
+ console.log(quiz.options)
+ console.log(quiz.control('c'))
+
+})
+
+
+function returnQuiz(questions) {
+    return questions[0];
+}
