ce9404f64fe39cf009887937a8aeadbc1e6cba6f
BekirLee <nihadbekirli8@gmail.com>
2023-05-19 20:27:27 +0400

    get quiz from js

--- Making quiz card/making_quizcard.js
+++ Making quiz card/making_quizcard.js
@@ -1,6 +1,6 @@
-function Question(text, options, right_answer) {
+function Question(id,text, options, right_answer) {
 
-
+    this.id = id;
     this.text = text;
     this.options = options;
     this.right_answer = right_answer;
@@ -13,16 +13,13 @@ function Question(text, options, right_answer) {
 
 
 let questions = [
-    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
+    new Question(1, "Which is program language", { a: "python", b: "java", c: "C", }, "c"),
 
 
-    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
-
-
-    new Question("Which is program language", { a: "python", b: "java", c: "C", }, "c"),
-
-
+  
 ]
+
+bring_quiz();
 
 // /0,undefined,null,'',"", -- false  ////// [],{}
 
@@ -45,4 +42,55 @@ document.querySelector(".btn-start").addEventListener("click", function () {
 
 function returnQuiz(questions) {
     return questions[0];
+}
+
+function bring_quiz() {
+    let bringer = document.getElementById("quizes")
+
+    for (taken_question of questions) {
+        let quiz = `
+        <header>
+        <div class="head">
+            <h1 class="title">Quiz test</h1>
+        </div>
+         </header>
+
+    <section id="${taken_question.id}" class="card-body">
+        <div class="question">${taken_question.text}</div>
+        <div class="option-list">
+            <div class="option  correct">
+                <div class="inner_option">
+                    <span>${taken_question.options} </span>
+                    <div class="icon"> <i class="fas fa-check"></i></div>
+                </div>
+
+            </div>
+
+            <div class="option incorrect">
+                <div class="inner_option">
+                    <span>${taken_question.options}</span>
+                    <div class="icon"> <i class="fas fa-times"></i></div>
+                </div>
+
+            </div>
+
+            <div class="option  incorrect">
+                <div class="inner_option">
+                    <span>${taken_question.options}</span>
+                    <div class="icon"> <i class="fas fa-times"></i></div>
+                </div>
+
+            </div>
+
+            <div class="option  incorrect">
+                <div class="inner_option">
+                    <span> ${taken_question.options}</span>
+                    <div class="icon"> <i class="fas fa-times"></i></div>
+                </div>
+
+            </div>
+        </div>
+     </section> `;
+        bringer.insertAdjacentHTML("beforeend", quiz);
+    }
 }
