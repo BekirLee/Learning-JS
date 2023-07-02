c6457bb56a249b1d85d30dd4375a4583a841bf99
BekirLee <nihadbekirli8@gmail.com>
2023-07-02 22:38:33 +0400

    learning some string methods

--- repeat/javasvcript.js
+++ repeat/javasvcript.js
@@ -1,5 +1,22 @@
 let ad = "Nihad is my name!";
 
-let tag;
+let tag = /Nihad/;
 
-tag = ad.charAt()
+tag = ad.charAt();
+tag = ad.charCodeAt();
+tag = ad.endsWith();
+tag = ad.includes('i');
+tag = ad.indexOf('a');
+tag = ad.length;
+tag = ad.normalize('NFKD');
+tag = ad.match("a");
+tag = ad.startsWith("N");
+tag = ad.valueOf();
+tag = ad.toUpperCase();
+tag = ad.toLocaleLowerCase();
+tag = ad.toLocaleUpperCase();
+tag = ad.slice("a");
+let tags = ad.match(tag);
+
+
+console.log(tags);
