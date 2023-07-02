77060c79cf841020ae7f1d19a4a2bc8fa9322cf9
BekirLee <nihadbekirli8@gmail.com>
2023-07-02 23:07:12 +0400

    numbers

---
+++ repeat/numbers.js
@@ -0,0 +1,12 @@
+let nmb1 = 10.2132434;
+let result;
+
+
+result = nmb1.toPrecision(5);
+result = nmb1.toPrecision(5);
+result = Math.floor(nmb1);
+//sqrt-->kok alti 
+//pow-->quvvet
+//abs-->modul
+
+console.log(result);
