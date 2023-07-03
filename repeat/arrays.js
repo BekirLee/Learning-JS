f8d7a56289f65aacea89fcd29d48917791a0e7d5
BekirLee <nihadbekirli8@gmail.com>
2023-07-03 23:22:25 +0400

    arrays

---
+++ repeat/arrays.js
@@ -0,0 +1,12 @@
+let array = ["apple", "orange", "pineapple", "banana"];
+
+let result;
+result = array.length;
+result = array.unshift();
+console.log(array.includes("apple"));
+result = array.push("gilas");
+
+result = array.splice(3, 4);
+
+console.log(result);
+console.log(array);
