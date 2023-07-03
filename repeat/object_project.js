c8cf0091ad0ffcf92a91737f94231eb577d71a2a
BekirLee <nihadbekirli8@gmail.com>
2023-07-03 23:41:15 +0400

    adding

---
+++ repeat/object_project.js
@@ -0,0 +1,12 @@
+let products = {
+    name: "Iphone13",
+    gender: "smartphone",
+    price: "1200$",
+    brand: "Apple"
+
+};
+let task;
+
+document.getElementById("text").innerHTML = task;
+
+console.log(products.gender);
