1308a7c46cefb3921cda2bca5a9e3f73bfdc6aea
BekirLee <nihadbekirli8@gmail.com>
2023-07-09 21:22:16 +0400

    again

--- QUIZ_APP/script.js
+++ QUIZ_APP/script.js
@@ -2,19 +2,29 @@ function Question(questiontext, options, correctAnswer) {
     this.questiontext = questiontext;
     this.options = options;
     this.correctAnswer = correctAnswer;
-    console.log(this);
+   
 }
 
 Question.prototype.checkAnswer = function (check) {
-    return (check) === this.correctAnswer;
+    return check === this.correctAnswer;
 }
 
 let questions = [
-    new Question("What web developer should know?",{a:"What web developer should know?",b})
-]
+    new Question("1=What web developer should know?", { a: "What web developer should know?", b: "What web developer should know?", c: "What web developer should know?" }, "c"),
+    new Question("2-What web developer should know?", { a: "What web developer should know?", b: "What web developer should know?", c: "What web developer should know?" }, "c"),
+    new Question("3-What web developer should know?", { a: "What web developer should know?", b: "What web developer should know?", c: "What web developer should know?" }, "c")
+];
 
+function QUIZ(questions) {
+    this.questions = questions;
+    this.questionIndex = 0;
+}
 
+QUIZ.prototype.getQuestion = function () {
+    return this.questions[this.questionIndex];
+}
 
+const quiz = new QUIZ(questions);
 
+console.log(quiz.getQuestion());
 
-console.log(question1.checkAnswer("c"));
